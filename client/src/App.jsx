d234f16d77e5762968e79abfa801bb4d7e4adb72
SahilV14 <sahil.vedpathak@spit.ac.in>
2023-03-31 18:59:56 +0530

    removed sample ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,10 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-    </EthProvider>
-  );
+    <div id="App">
+
+    </div>
+  )
 }
 
 export default App;
