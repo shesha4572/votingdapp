ebe4b7485b623b7ee4cc3bec208f17819d11de48
SahilV14 <sahil.vedpathak@spit.ac.in>
2023-03-31 20:16:54 +0530

    login-1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-
+import Login from "./components/Login";
 function App() {
   return (
     <div id="App">
-
+    <Login />
     </div>
   )
 }
-
 export default App;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import logo from "../images/voting-logo.png";
+export default function Login() {
+    return(
+        <>
+        <div id="mid-box">
+            <div id="login-left">
+                {/*<img src={logo} />*/}
+                <p>left</p>
+            </div>
+
+            <div id="login-right">
+                <p>right</p>
+            </div>
+        </div>
+        </>
+    )
+}
