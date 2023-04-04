35d58892d05a194ab6668919d3a7fb187f7bfb32
SahilV14 <sahil.vedpathak@spit.ac.in>
2023-04-04 14:55:05 +0530

    Fixed DOB label overlap, added required field for login and register forms

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -30,20 +30,20 @@ export default function Register () {
         <div id="Form-top">
 
             <div id="Form-top-left">
-                <TextField className="Text-input" sx={input} variant='standard' type="text"  id="First-name" label="First Name" />
-                <TextField className="Text-input" sx={input} variant='standard' type="date"  id="DOB" label="Date of BIrth"/>
+                <TextField className="Text-input" sx={input} variant='standard' type="text"  id="First-name" label="First Name" required/>
+                <TextField className="Text-input" sx={input} variant='standard' type="date"  id="DOB" label="Date of Birth" value='1' required/>
             </div>
 
             <div id="Form-top-right">
-                <TextField className="Text-input" sx={input} variant='standard' type="text" id="Last-name" label="Last Name" />
-                <TextField className="Text-input" sx={input} variant='standard' type="number" id="Phone-no" label="Contact Number" />
+                <TextField className="Text-input" sx={input} variant='standard' type="text" id="Last-name" label="Last Name" required/>
+                <TextField className="Text-input" sx={input} variant='standard' type="number" id="Phone-no" label="Contact Number" required/>
             </div>
         </div>
 
         <div id="Form-bottom">
-            <TextField className="Text-input2" sx={input} variant='standard' type="text" id="number" label="Aadhar number"/>
-            <TextField className="Text-input2" sx={input} variant='standard' type="text" id="Email" label="Email Address"/>
-            <TextField className="Text-input2" sx={input} variant='standard' type="password" id="Password" label="Create a password" />
+            <TextField className="Text-input2" sx={input} variant='standard' type="text" id="Email" label="Email Address" required/>
+            <TextField className="Text-input2" sx={input} variant='standard' type="text" id="number" label="Aadhar number" required/>
+            <TextField className="Text-input2" sx={input} variant='standard' type="password" id="Password" label="Create a password" required/>
         </div>
         <Button id="Register-button" variant='contained' sx={btn}>Register</Button>
     </div>
